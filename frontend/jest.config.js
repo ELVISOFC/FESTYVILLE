ee01cc9bd1b3f989eb04b787195f8afab2148dce
ELVISOFC <parodyrull@gmail.com>
2026-05-17 15:02:02 -0400

    FESTYVILLE 517
    CHANGES AS OF FIRST WEEK OF DEVELOPMENT FOR FESTYVILLE 5/14/2026

--- frontend/jest.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('jest').Config} */
-module.exports = {
-  testEnvironment: "node",
-  testMatch: ["**/src/lib/**/*.test.ts"],
-  transform: {
-    "^.+\\.tsx?$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.test.json" }],
-  },
-};
